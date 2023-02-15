9da4760c2b287bb561faea83da853efaeace6c5f
vflorin1995 <vflorin1995@gmail.com>
2023-02-15 16:21:14 +0200

    Add integration tests

---
+++ spec/request/categories_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe GroupsController, type: :request do
+  describe 'GET categories path' do
+    subject { User.new(Name: 'JOhn', email: 'qq@qq.qq', password: '123456') }
+
+    before { subject.save }
+
+    before(:each) do
+      get groups_path
+    end
+
+    it 'should redirect to sign_up' do
+      Group.create(Name: 'Fuel', AuthorId: subject.id, Icon: "www.google.com/cat.jpg")
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+end
